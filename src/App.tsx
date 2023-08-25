f3c4e827c5a8de41441e22dde59a6f5e36f123bf
Jürgen Mikk <jurgen.mikk@khk.ee>
2023-08-25 18:50:15 +0300

    DT-implemented-search-dictionary-definition-request-handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,48 @@
 import React, {useEffect, useState} from 'react';
 import './App.scss';
+import {BiSearch} from 'react-icons/bi';
+import {handleGetDictionaryResponse} from "./services/dictionaryService";
+import {AxiosError} from 'axios';
 import MenuBar from "./components/MenuBar/MenuBar";
 
 function App() {
 
     const [font, setFontType] = useState<string>('Sans-Serif');
     const [isThemeDark, setTheme] = useState<boolean>(false);
+    const [search, setSearch] = useState<string>('');
+    const [searchResult, setResult] = useState<unknown>();
+
+    const dictionaryUrl = `${process.env.REACT_APP_GET_DEFINITION}/${search}`;
+
+    const handleSearchKeywordChange = (word: string) => {
+        setSearch(word);
+    }
+
+    const handleSearchSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        try {
+            e.preventDefault();
+
+            const dictionaryResponse = await handleGetDictionaryResponse(dictionaryUrl);
+            setResult(dictionaryResponse);
+        } catch (error) {
+
+            if (error instanceof AxiosError) {
+                const requestError = error.response?.data;
+                setResult(requestError);
+            } else {
+                console.log('Error trying to request search definition:', error);
+            }
+        }
+    }
+
+    const handleApplicationThemeChange = () => {
+        document.body.style.backgroundColor = isThemeDark
+            ? '#050505'
+            : 'white';
+    }
 
     useEffect(() => {
-        document.body.style.backgroundColor = isThemeDark ? '#050505' : 'white';
+        handleApplicationThemeChange();
     }, [isThemeDark])
 
   return (
@@ -19,6 +53,21 @@ function App() {
             setFontType={setFontType}
             setTheme={setTheme}
         />
+        <form onSubmit={(e) =>handleSearchSubmit(e)}>
+            <input
+                name="search"
+                type="text"
+                placeholder="Search for any word..."
+                className={isThemeDark ? 'field-dark' : ''}
+                onChange={(e) =>
+                    handleSearchKeywordChange(e.target.value)
+                }
+            />
+            <BiSearch
+                id="search-icon"
+                type="submit"
+            />
+        </form>
     </div>
   );
 }
